3cd483865b79de18efbfd2f0529ca7cc7a070670
Renan <renan.sim@icloud.com>
2025-12-17 17:46:20 +0000

    feat(app): adicionar estrutura inicial do aplicativo CortexOS com interface e configurações

---
+++ examples/ios-app/ios/CortexOS/CortexOS/CortexOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CortexOSApp.swift
+//  CortexOS
+//
+//  Created by Renan Simões on 17/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct CortexOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
